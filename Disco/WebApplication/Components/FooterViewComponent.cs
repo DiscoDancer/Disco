499480622718acb8e0d4f2b593d0c577415d903d
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-05-17 20:43:57 +0300

    #2 header, footer
    - base components with styles

---
+++ Disco/WebApplication/Components/FooterViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models.ViewModels.Footer;
+
+namespace WebApplication.Components
+{
+    public class FooterViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new FooterViewModel
+            {
+                CurrentYear = DateTime.Now.Year,
+                CompanyName = "Disco"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Disco/WebApplication/Models/ViewModels/Footer/FooterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication.Models.ViewModels.Footer
+{
+    public class FooterViewModel
+    {
+        public int CurrentYear { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
