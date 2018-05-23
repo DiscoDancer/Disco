74a52f65a6ca5370c564e389dd89c5f64c4392e7
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-05-23 11:09:39 +0300

    #6 gulp
    - added task for bundling styles

---
+++ Disco/WebApplication/gulpfile.js
@@ -0,0 +1,11 @@
+﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task("bundle-styles", function () {
+    return gulp.src("wwwroot/styles/**/*.css")
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(concat("bundle.css"))
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("wwwroot/styles/dist"));
+});
