c564ca65005faec732d93e3a62e4289405f9ee3a
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-05-23 14:16:08 +0300

    #10 Clear bundle.css after each build
    - done

--- Disco/WebApplication/gulpfile.js
+++ Disco/WebApplication/gulpfile.js
@@ -1,8 +1,12 @@
 ﻿var gulp = require("gulp");
 var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
+var del = require("del");
 
 gulp.task("bundle-styles", function () {
+    del("wwwroot/styles/dist/bundle.css");
+    del("wwwroot/styles/dist/bundle.css.map");
+
     return gulp.src("wwwroot/styles/**/*.css")
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(concat("bundle.css"))
