86fb3b6824ba3b23e0116ab663b8b0ebfa7650d4
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-05-23 19:29:18 +0300

    #11 webpack
    - webpack and babel are configured for the project
    - removed tmp file and updated gitignore

--- Disco/WebApplication/gulpfile.js
+++ Disco/WebApplication/gulpfile.js
@@ -1,6 +1,10 @@
 ﻿var gulp = require("gulp");
+
 var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
+
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config.js");
 
 gulp.task("bundle-styles", function () {
     return gulp.src("wwwroot/styles/src/**/*.css")
@@ -8,4 +12,9 @@ gulp.task("bundle-styles", function () {
         .pipe(concat("bundle.css"))
         .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest("wwwroot/styles/dist"));
+});
+
+gulp.task("bundle-scripts", function () {
+    webpack(webpackConfig, function (err, stats) {
+    });
 });
---
+++ Disco/WebApplication/webpack.config.js
@@ -0,0 +1,23 @@
+﻿var path = require("path");
+var glob = require("glob");
+
+module.exports = {
+    entry:
+        glob.sync("./wwwroot/scripts/src/**/*.js"),
+    output: {
+        path: path.resolve(__dirname, "wwwroot/scripts/dist"),
+        filename: "bundle.js"
+    },
+    devtool: "source-maps",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
