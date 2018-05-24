7e54d615dcbf1f558f96767271f39c8e7ae0eaf8
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-05-24 20:31:57 +0300

    #15 timer bones
    - added more empty pages
    - styles are renamed

--- Disco/WebApplication/Controllers/TimerController.cs
+++ Disco/WebApplication/Controllers/TimerController.cs
@@ -5,5 +5,13 @@ namespace WebApplication.Controllers
     public class TimerController : Controller
     {
         public ViewResult Index() => View();
+
+        public ViewResult Settings() => View();
+
+        public ViewResult History() => View();
+
+        public ViewResult BrowseActivities() => View();
+
+        public ViewResult EditActivity() => View();
     }
 }
