4e73eb411df8fcf63ca0d0e5b2b9b8a49b050d97
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-07 10:18:40 +0300

    #25 sounds
    - dev progress

---
+++ Disco/WebApplication/TagHelpers/PlaySoundTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApplication.TagHelpers
+{
+    public class PlaySoundTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context,
+            TagHelperOutput output)
+        {
+            var result = new TagBuilder("span");
+
+            var playTag = CreatePlayButton();
+            result.InnerHtml.AppendHtml(playTag);
+
+            output.Content.AppendHtml(result.InnerHtml);
+            output.TagName = "span";
+        }
+
+        private TagBuilder CreatePlayButton()
+        {
+            var editTag = new TagBuilder("a");
+            editTag.Attributes["href"] = "google.com";
+            editTag.AddCssClass("anchor-no-visually-effects");
+
+            var editTagSpan = new TagBuilder("span");
+            editTagSpan.AddCssClass("play-icon");
+            editTagSpan.InnerHtml.Append("|>");
+
+            editTag.InnerHtml.AppendHtml(editTagSpan);
+
+            return editTag;
+        }
+    }
+}
