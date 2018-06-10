c42989493a7a1f734dd78aecca9a1bbb20a205be
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-10 15:58:25 +0300

    #25 sounds
    - player works

--- Disco/WebApplication/TagHelpers/PlaySoundTagHelper.cs
+++ Disco/WebApplication/TagHelpers/PlaySoundTagHelper.cs
@@ -1,35 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.TagHelpers
 {
     public class PlaySoundTagHelper : TagHelper
     {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public int SoundId { get; set; }
+
+        public PlaySoundTagHelper(IUrlHelperFactory helperFactory)
+        {
+            _urlHelperFactory = helperFactory;
+        }
+
         public override void Process(TagHelperContext context,
             TagHelperOutput output)
         {
             var result = new TagBuilder("span");
 
-            var playTag = CreatePlayButton();
-            result.InnerHtml.AppendHtml(playTag);
+            var playButton = CreatePlayButton();
+            var playControls = CreatePlayControls();
+            result.InnerHtml.AppendHtml(playButton);
+            result.InnerHtml.AppendHtml(playControls);
 
             output.Content.AppendHtml(result.InnerHtml);
             output.TagName = "span";
         }
 
+        private TagBuilder CreatePlayControls()
+        {
+            var audio = new TagBuilder("audio");
+            audio.Attributes["controls"] = "controls";
+
+            var source = new TagBuilder("source");
+            audio.Attributes["type"] = "audio/mpeg";
+            audio.Attributes["src"] = AudioUrl;
+
+            const string audioNotAvaliable = "Your browser does not support the audio element.";
+
+            audio.InnerHtml.AppendHtml(source);
+            audio.InnerHtml.Append(audioNotAvaliable);
+
+            return audio;
+        }
+
+        private string AudioUrl
+        {
+            get
+            {
+                var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+                return urlHelper.Action("GetSoundById",
+                    new
+                    {
+                        id = SoundId
+                    });
+            }
+        }
+
         private TagBuilder CreatePlayButton()
         {
-            var editTag = new TagBuilder("a");
-            editTag.Attributes["href"] = "google.com";
-            editTag.AddCssClass("anchor-no-visually-effects");
+            var btn = new TagBuilder("input");
+            btn.Attributes["type"] = "button";
+            btn.AddCssClass("play-icon");
+            btn.Attributes["value"] = "|>";
 
-            var editTagSpan = new TagBuilder("span");
-            editTagSpan.AddCssClass("play-icon");
-            editTagSpan.InnerHtml.Append("|>");
-
-            editTag.InnerHtml.AppendHtml(editTagSpan);
-
-            return editTag;
+            return btn;
         }
     }
 }
---
+++ Disco/WebApplication/wwwroot/scripts/src/timer/edit-sounds.js
@@ -0,0 +1,6 @@
+﻿const playIcon = document.getElementsByClassName("play-icon")[0];
+const audio = document.getElementsByTagName("audio")[0];
+
+playIcon.addEventListener("click", () => {
+    audio.play();
+});
